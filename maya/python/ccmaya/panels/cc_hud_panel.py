3f9674a2c1b4b7fda2bffc9c0800f3f7daa4000b
Lebowski23 <joe@no8london.com>
2026-04-05 17:19:03 +0100

    work on the controls for the hud

--- maya/python/ccmaya/panels/cc_hud_panel.py
+++ maya/python/ccmaya/panels/cc_hud_panel.py
@@ -26,8 +26,10 @@ class ControlChaosHUDPanel(base_ui.WidgetBase):
         super().__init__(parent)
         self.populate_data()
         self.populate_speed_objects()
-        self.update_controls()
+        self.update_cam_controls()
+        self.update_speed_controls()
         self.connect_signals()
+        self.lbl_header.setHidden(True)
 
     def populate_data(self):
         font_size = cmds.getAttr(f"{self.speed_node}.font_size")
@@ -51,16 +53,49 @@ class ControlChaosHUDPanel(base_ui.WidgetBase):
         self.cmb_speed_unit.setCurrentIndex(speed_unit_index)
 
     def connect_signals(self):
+        # connect the camera controls
+        self.sld_cam_text_scale.valueChanged.connect(self.set_cam_text_scale)
+        self.sld_cam_y_offset.valueChanged.connect(self.set_cam_y_offset)
+
+        # connect the speed controls
         self.sp_font_size.valueChanged.connect(self.set_font_size)
         self.cmb_font_type.currentIndexChanged.connect(self.set_font_type)
         self.cmb_font_weight.currentIndexChanged.connect(self.set_font_weight)
         self.cmb_speed_unit.currentIndexChanged.connect(self.set_speed_unit)
         self.btn_add_selected.clicked.connect(self.add_selected_object)
-        self.cmb_speed_objects.currentIndexChanged.connect(self.update_controls)
+        self.cmb_speed_objects.currentIndexChanged.connect(self.update_speed_controls)
         self.sld_x_offset.valueChanged.connect(self.set_x_sb)
         self.sld_y_offset.valueChanged.connect(self.set_y_sb)
         self.chk_visible.toggled.connect(self.set_visible)
         self.btn_text_colour.clicked.connect(self.set_colour)
+
+    @property
+    def cam_node(self):
+        cc_cam_node = cmds.ls(type="controlChaosHUD")
+        if cc_cam_node:
+            return cc_cam_node[0]
+        if not cmds.pluginInfo("controlChaosHUD.py", query=True, loaded=True):
+            cmds.loadPlugin("controlChaosHUD.py")
+        return cmds.createNode("controlChaosHUD")
+
+    @property
+    def speed_node(self):
+        cc_speed_node = cmds.ls(type="objectSpeedHUD")
+        if cc_speed_node:
+            return cc_speed_node[0]
+        if not cmds.pluginInfo("objectSpeedHUD.py", query=True, loaded=True):
+            cmds.loadPlugin("objectSpeedHUD.py")
+        return cmds.createNode("objectSpeedHUD")
+
+    def set_cam_text_scale(self):
+        cam_text_scale = self.sld_cam_text_scale.value() / 100
+        self.sb_cam_text_scale.setValue(cam_text_scale)
+        cmds.setAttr(f"{self.cam_node}.overall_text_scale", cam_text_scale)
+
+    def set_cam_y_offset(self):
+        cam_y_offset = self.sld_cam_y_offset.value()
+        self.sb_cam_y_offset.setValue(cam_y_offset)
+        cmds.setAttr(f"{self.cam_node}.text_y_offset", cam_y_offset)
 
     def set_visible(self):
         visible = self.chk_visible.isChecked()
@@ -86,15 +121,6 @@ class ControlChaosHUDPanel(base_ui.WidgetBase):
         self.sb_y_offset.setValue(y_offset)
         index = self.get_object_index()
         cmds.setAttr(f"{self.speed_node}.text_y_offset{index}", y_offset)
-
-    @property
-    def speed_node(self):
-        cc_speed_node = cmds.ls(type="objectSpeedHUD")
-        if cc_speed_node:
-            return cc_speed_node[0]
-        if not cmds.pluginInfo("objectSpeedHUD.py", query=True, loaded=True):
-            cmds.loadPlugin("objectSpeedHUD.py")
-        return cmds.createNode("objectSpeedHUD")
 
     def get_speed_object_list(self):
         speed_object_list = list()
@@ -159,19 +185,23 @@ class ControlChaosHUDPanel(base_ui.WidgetBase):
     def set_colour(self):
         colour = QtWidgets.QColorDialog.getColor()
         r, g, b, _ = colour.getRgb()
-        print (colour.getRgb())
         stylesheet = f"background-color: rgb({r}, {g}, {b});"
         self.btn_text_colour.setStyleSheet(stylesheet)
 
         index = self.get_object_index()
         attribute_name = f"{self.speed_node}.speed_text_colour{index}"
-        print ((r/255), (g/255), (b/255))
         cmds.setAttr(attribute_name, (r/255), (g/255), (b/255), type="double3")
 
+    def update_cam_controls(self):
+        cam_text_scale = cmds.getAttr(f"{self.cam_node}.overall_text_scale")
+        self.sld_cam_text_scale.setValue(cam_text_scale * 100)
+        self.sb_cam_text_scale.setValue(cam_text_scale)
 
+        cam_y_offset = cmds.getAttr(f"{self.cam_node}.text_y_offset")
+        self.sld_cam_y_offset.setValue(cam_y_offset)
+        self.sb_cam_y_offset.setValue(cam_y_offset)
 
-
-    def update_controls(self):
+    def update_speed_controls(self):
         index = self.get_object_index()
         if index == 0:
             return
@@ -189,6 +219,8 @@ class ControlChaosHUDPanel(base_ui.WidgetBase):
         stylesheet = self.get_stylesheet_from_attribute(attribute_name)
         self.btn_text_colour.setStyleSheet(stylesheet)
 
+
+'''
 def create_cc_panel():
     """
     Create the dockable panel in Maya
@@ -196,6 +228,7 @@ def create_cc_panel():
     create_dockable_widget.CreateDockableWidget(
         ControlChaosHUDPanel, "Control Chaos Hud", "Control Chaos Hud", 500
     )
+'''
 
 
 def main():
