9d68982aa7e3c0cad251af0fbb36b7763aa8e5a2
joeleveson <joeleveson@hotmail.com>
2026-04-06 19:13:02 +0100

    separate bat file for installing for maya

--- bat/install_env.bat
+++ bat/install_env.bat
@@ -4,8 +4,12 @@ SET CURRENT_ROOT=%~dp0
 SET "PIPELINE_ROOT=%CURRENT_ROOT:~0,-5%"
 FOR %%I IN ("%PIPELINE_ROOT%") DO SET ROOT_DIR=%%~dpI
 SET SITE_PACKAGES_311=%ROOT_DIR%virtual_env\python311
+SET SITE_PACKAGES_311_NOPYSIDE=%ROOT_DIR%virtual_env\python311_nopyside
 
 py -3.11 -m venv %SITE_PACKAGES_311%
 "C:/Python/python311/python.exe" -m pip install -r "%PIPELINE_ROOT%/requirements/requirements311.txt" --target %SITE_PACKAGES_311%\Lib\site-packages
+
+py -3.11 -m venv %SITE_PACKAGES_311_NOPYSIDE%
+"C:/Python/python311/python.exe" -m pip install -r "%PIPELINE_ROOT%/requirements/requirements311_nopyside.txt" --target %SITE_PACKAGES_311_NOPYSIDE%\Lib\site-packages
 
 pause
--- bat/launch_maya2024.bat
+++ bat/launch_maya2024.bat
@@ -2,7 +2,7 @@ SET CURRENT_ROOT=%~dp0
 SET "PIPELINE_ROOT=%CURRENT_ROOT:~0,-5%"
 FOR %%I IN ("%PIPELINE_ROOT%") DO SET ROOT_DIR=%%~dpI
 set PYTHONPATH=C:\Program Files\Autodesk\Maya2024\Python\Lib\site-packages
-set PYTHONPATH=%PYTHONPATH%;%ROOT_DIR%virtual_env\python311\Lib\site-packages
+set PYTHONPATH=%PYTHONPATH%;%ROOT_DIR%virtual_env\python311_nopyside\Lib\site-packages
 set PYTHONPATH=%PYTHONPATH%;%PIPELINE_ROOT%\core\python
 set PYTHONPATH=%PYTHONPATH%;%PIPELINE_ROOT%\maya\python
 
